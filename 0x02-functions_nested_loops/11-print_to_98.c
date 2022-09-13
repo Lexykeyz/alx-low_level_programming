255d7bc3c0c3819b1a2cb4cbfd5e0abfafd77b4c
LEXYKEYZ <essienalexbennet@gmail.com>
2022-09-13 15:15:08 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,8 +11,9 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-		printf("%d, ", n--);
+		printf("%d\n", n);
 	}
+
 
 	else
 	{
@@ -21,3 +22,4 @@ void print_to_98(int n)
 		printf("%d\n", n);
 	}
 }
+
