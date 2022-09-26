09b5ea14f776f383a8c066acb74275bd8a19f889
LEXYKEYZ <essienalexbennet@gmail.com>
2022-09-26 15:22:04 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,9 +3,9 @@
 /**
  * _strspn - gets lengthof a prefix substring
  * @s: string to check
- * @accept : string to check against
+ * @accept: string to check against
  *
- * Return : number of bytes of s in accept
+ * Return: number of bytes of s in accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
@@ -18,8 +18,8 @@ unsigned int _strspn(char *s, char *accept)
 			if (s[i] == accept[j])
 				break;
 		}
-			if (!accept[j])
-				break;
+		if (!accept[j])
+			break;
 	}
 	return (i);
 }
