2cad9b55e2b900d4f1ef49266598d54e732b4f94
LEXYKEYZ <essienalexbennet@gmail.com>
2022-10-04 12:25:21 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,25 +11,25 @@
 
 char *_strdup(char *str)
 {
-	char *cpy;
-	int index, len;
+        char *cpy;
+        int index, len;
 
-	if (str == NULL)
-		return (NULL);
+        if (str == NULL)
+                return (NULL);
 
-	for (index = 0; str[index]; index++)
-		len++;
-	cpy = malloc(sizeof(char) * (len + 1));
+        for (index = 0; str[index]; index++)
+                len++;
+        cpy = malloc(sizeof(char) * (len + 1));
 
-	if (cpy == NULL)
-		return (NULL);
+        if (cpy == NULL)
+                return (NULL);
 
-	for (index = 0; str[index]; index++)
-	{
-		cpy[index] = str[index];
-	}
+        for (index = 0; str[index]; index++)
+        {
+                cpy[index] = str[index];
+        }
 
-	cpy[len] = '\0';
+        cpy[len] = '\0';
 
-	return (cpy);
+        return (cpy);
 }
