7fb3e0f08c86f3cc6429203a9f17b28f521d1366
LEXYKEYZ <essienalexbennet@gmail.com>
2022-10-13 10:22:18 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -16,7 +16,7 @@ typedef struct print
 } print_t;
 
 int _putchar(char);
-int sum_them_all(const unsigned int n , ...);
+int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
